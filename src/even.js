0e843bf74b8f4a2050da515143b54fb6aca39698
Maxim <SaenkoJr@users.noreply.github.com>
2019-03-19 15:14:17 +0200

    fixed undefined const

--- src/even.js
+++ src/even.js
@@ -25,7 +25,7 @@ export default () => {
 
     const correctAnswer = isEven(gameQuestionNum) ? 'yes' : 'no';
 
-    if (answer !== correctAnswer) {
+    if (userAnswer !== correctAnswer) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${userName}!`);
       return;
     }
