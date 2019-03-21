d21c5d8ad7f5a1093ede830341444154a74a6a7c
Maksim Saenko <mm.slim.one@gmail.com>
2019-03-21 11:50:43 +0200

    refactored progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,32 +1,34 @@
 import buildGame from '../core';
 import getRandomNum from '../utils';
 
-const makeProgression = (initValue, step, length) => {
+const length = 10;
+
+const makeProgression = (initValue, step, hiddenValueIndex) => {
   const iter = (acc, index) => {
     if (index === length) return acc;
-    const nextNumber = initValue + step * index;
-    const newAcc = [...acc, nextNumber];
+
+    if (index === hiddenValueIndex) {
+      return iter(`${acc} ..`, index + 1);
+    }
+
+    const nextValue = initValue + step * index;
+    const newAcc = `${acc} ${nextValue}`;
+
     return iter(newAcc, index + 1);
   };
 
-  return iter([], 0);
+  return iter('', 0);
 };
-
-const length = 10;
 
 const gameDescription = 'Find the greatest common divisor of given numbers.';
 const makeGameProcess = () => {
   const step = getRandomNum(1, 15);
   const initValue = getRandomNum(0, 100);
 
-  const progression = makeProgression(initValue, step, length);
-
   const hiddenValueIndex = getRandomNum(0, length - 1);
   const hiddenValue = initValue + step * hiddenValueIndex;
 
-  const gameQuestion = progression.reduce((acc, value) => (
-    value === hiddenValue ? `${acc} ..` : `${acc} ${value}`
-  ), '');
+  const gameQuestion = makeProgression(initValue, step, hiddenValueIndex);
   const correctAnswer = hiddenValue.toString();
 
   return { gameQuestion, correctAnswer };
